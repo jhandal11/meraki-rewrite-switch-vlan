ccd2fdf9a4ccaab43a384325852635838e775b4f
nathanwiens <nathan.wiens@gmail.com>
2018-06-06 10:30:49 -0600

    Update SwitchRewriteVLAN.py

--- SwitchRewriteVLAN.py
+++ SwitchRewriteVLAN.py
@@ -1,3 +1,10 @@
+### SwitchRewriteVLAN.py
+# If a client matches the ip_address_string and the VLAN matches source_vlan,
+# this script will change the port VLAN to dest_VLAN
+# 
+# Written by Nathan Wiens (nathan@wiens.co)
+###
+
 import json
 import os
 import sys
